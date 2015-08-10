1f1ba0eb28091dca472cb262220421e5d1c5262e
Nikki <oathofrose@gmail.com>
2015-08-10 18:59:50 +0800

    Adjusted Models; Admin side can now add Items with sizes and color

--- thesis/modifit/admin.py
+++ thesis/modifit/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Item, hasSize
+
+class hasSizeInline(admin.TabularInline):
+	model = hasSize
+	extra = 1
+
+class hasColorInline(admin.TabularInline):
+	model = hasColor
+	extra = 1
+
+class ItemAdmin(admin.ModelAdmin):
+	inlines = [hasSizeInline, hasColorInline]
+
+admin.site.register(Item, ItemAdmin)
--- thesis/modifit/models.py
+++ thesis/modifit/models.py
@@ -5,13 +5,13 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 
 class UserAvatar(models.Model):
-	user_id = models.ForeignKey(User)
-	shoulder = models.PositiveIntegerField(default=15)
-	bust = models.PositiveIntegerField(default=34)
-	waist = models.PositiveIntegerField(default=24)
-	hips = models.PositiveIntegerField(default=34)
+	user = models.ForeignKey(User)
+	shoulder = models.DecimalField(max_digits=4, decimal_places=1, default=15, null=True, blank=True)
+	bust = models.DecimalField(max_digits=4, decimal_places=1, default=34, null=True, blank=True)
+	waist = models.DecimalField(max_digits=4, decimal_places=1, default=24, null=True, blank=True)
+	hips = models.DecimalField(max_digits=4, decimal_places=1, default=34, null=True, blank=True)
 	#length = models.PositiveIntegerField(default=0)
-	height = models.PositiveIntegerField(default=68)
+	height = models.DecimalField(max_digits=4, decimal_places=1, default=68, null=True, blank=True)
 
 	SKINTONE_CHOICES = (
 		('W', "Warm"),
@@ -21,42 +21,61 @@ class UserAvatar(models.Model):
 	)
 	skintone = models.CharField(max_length=1, choices=SKINTONE_CHOICES)
 
-class Wardrobe(models.Model):
-	user_id = models.ForeignKey(User)
-	item_id = models.ForeignKey(Item)
-	times_used = models.PositiveIntegerField(default=0)
 
 class Item(models.Model):
 	item_name = models.CharField(max_length=100)
-	shoulder = models.PositiveIntegerField(default=0)
-	bust = models.PositiveIntegerField(default=0)
-	waist = models.PositiveIntegerField(default=0)
-	hips = models.PositiveIntegerField(default=0)
-	length = models.PositiveIntegerField(default=0)
+
+	def __unicode__(self):
+		return self.item_name
+
+class hasSize(models.Model):
+	item = models.ForeignKey(Item)
+	SIZE_CHOICES = (
+		("XXS", "Extra, Extra Small"),
+		("XS", "Extra Small"),
+		("S", "Small"),
+		("M", "Medium"),
+		("L", "Large"),
+		("XL", "Extra Large"),
+		("XXL", "Extra, Extra Large"),
+		("One", "One Size Fits All"),
+	)
+	size = models.CharField(max_length=3, choices=SIZE_CHOICES)
+	shoulder = models.DecimalField(max_digits=4, decimal_places=1, default=0, null=True, blank=True)
+	bust = models.DecimalField(max_digits=4, decimal_places=1, default=0, null=True, blank=True)
+	waist = models.DecimalField(max_digits=4, decimal_places=1, default=0, null=True, blank=True)
+	hips = models.DecimalField(max_digits=4, decimal_places=1, default=0, null=True, blank=True)
+	length = models.DecimalField(max_digits=4, decimal_places=1, default=0, null=True, blank=True)
 
 class hasColor(models.Model):
-	item_id = models.ForeignKey(Item)
+	item = models.ForeignKey(Item)
 	red = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(255)])
 	green = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(255)])
 	blue = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(255)])
 
-class hasPattern(models.Model):
-	item_id = models.ForeignKey(Item)
-	pattern_id = models.ForeignKey(Patterns)
-
 class Patterns(models.Model):
 	pattern_name = models.CharField(max_length=100, unique=True)
 
-class hasMaterials(models.Model):
-	item_id = models.ForeignKey(Item)
-	material_id = models.ForeignKey(Materials)
+class hasPattern(models.Model):
+	item = models.ForeignKey(Item)
+	pattern = models.ForeignKey(Patterns)
 
 class Materials(models.Model):
 	material_name = models.CharField(max_length=100,unique=True)
 
-class hasCategory(models.Model):
-	item_id = models.ForeignKey(Item)
-	category_id = models.ForeignKey(Categories)
+class hasMaterials(models.Model):
+	item = models.ForeignKey(Item)
+	material = models.ForeignKey(Materials)
 
 class Categories(models.Model):
 	category_name = models.CharField(max_length=150, unique=True)
+
+class hasCategory(models.Model):
+	item = models.ForeignKey(Item)
+	category = models.ForeignKey(Categories)
+
+
+class Wardrobe(models.Model):
+	user = models.ForeignKey(User)
+	item = models.ForeignKey(Item)
+	times_used = models.PositiveIntegerField(default=0)
