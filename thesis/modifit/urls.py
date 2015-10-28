0c9d16b290f3cc709882381e8e2b510f6cd3a682
Nikki <nikki.ebora@gmail.com>
2015-10-28 21:03:39 +0800

    Adjusted the index view to a login page.

--- thesis/modifit/urls.py
+++ thesis/modifit/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^login/$', views.index, name='index'),
+    url(r'^home/$', views.home, name='home'),
+    #url(r'^$', views.home, name='home'),
     url(r'^(?P<wardrobe_id>[0-9]+)/$', views.wardrobe, name='wardrobe'),
 ]
