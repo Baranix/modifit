2b7790c4f0d97591f195afbfd782a7cdf68f9896
Nikki <nikki.ebora@gmail.com>
2015-10-28 21:00:53 +0800

    Adjusted cosine_similarity.py to use data from database. Implemented kNN.

--- thesis/modifit/cosine_similarity.py
+++ thesis/modifit/cosine_similarity.py
@@ -1,9 +1,11 @@
 # cosine similarity
 
 import numpy as np
-import math
+from math import sqrt
+#import time
+import db_connector
 
-users = { 
+"""users = { 
 	"Angelica" : {
 		"Printed Cotton T-Shirt" 				: 5,
 		"High Neck Tank" 						: 2,
@@ -49,13 +51,130 @@ users = {
 		"Contrast Panel Cut In Dress"			: 2,
 		"Fitted Dress"							: 5
 	}
-}
+}"""
 
-def get_similarity( user1, user2 ):
+def getRecItems(currentUser, weights, users):
+	itemRatings = {}
+	#items = {}
+	totalWeight = 0
+	for k in weights.keys():
+		#print str(k) + ": " + str(weights[k])
+		#items[k] = users[k]
+		totalWeight = totalWeight + weights[k]
+
+	for i in range( 1, 31 ):
+		if users[currentUser+2][i] == 0:
+			projectedRating = 0
+			for k in weights.keys():
+				"""print "User #" + str(k) + " and Item #" + str(i) + ":"
+				print "Neighbor's Rating: " + str(users[k+2][i])
+				print "Weight: " + str(weights[k])"""
+				normalizedRating = users[k+2][i] * weights[k]
+				"""print "Normalized Rating: " + str(normalizedRating)"""
+				projectedRating = projectedRating + normalizedRating
+
+			itemRatings[i] = projectedRating
+
+	"""print
+	print "PROJECTED RATING: " + str(projectedRating)
+	print"""
+
+	return itemRatings
+
+
+def kNN( user, k ):
+	k = k*(-1)
+	top = user.argsort()[k:][::-1]
+
+	return top
+
+def assignWeights( user, NN, matrix ):
+	totalWeight = 0
+	#print "Nearest neighbors:"
+	#print NN
+	for n in NN:
+		#print "User " + str(n) + ": " + str(matrix[user][n])
+		totalWeight = totalWeight + matrix[user][n]
+
+	#print "Total weight: " + str(totalWeight)
+	weights = {}
+	for n in NN:
+		weights[n] = (matrix[user][n] / totalWeight)
+
+	return weights
+
+
+def getSimilarity( user1, user2 ):
 	dot = np.dot(user1, user2)
-	vectors = math.sqrt( np.sum( user1**2 ) ) * math.sqrt( np.sum( user2**2 ) )
-	print dot / vectors
+	vectors = sqrt( np.sum( user1**2 ) ) * sqrt( np.sum( user2**2 ) )
+	result = dot / vectors
+
+	return result
+
+def getOrderedValues( user1, user2 ):
+	for k in user2.keys():
+		if k not in user1:
+			user1[k] = 0
+	a = []
+	for k in sorted(user1):
+		a.append(user1[k])
+
+	return a
+
+def getMatrix( users ):
+	matrix = []
+	for k in range( 2, len(users)+2 ):
+		row = []
+		for k2 in range( 2, len(users)+2 ):
+			if k==k2:
+				row.append(1)
+			else:
+				a = getOrderedValues( users.get(k), users.get(k2) )
+				b = getOrderedValues( users.get(k2), users.get(k) )
+				row.append( getSimilarity( np.array( a ), np.array( b ) ) )
+		matrix.append(row)
+
+	return matrix
 
 def main():
-	print "Angelica and Billie Jean: "
-	get_similarity( users.get("Angelica").values(), users.get("Billie Jean").values() )
+	users = db_connector.getAllWardrobeData()
+
+	#start_time = time.time()
+
+	matrix = getMatrix( users )
+	#print users[2]
+	#u = input("Enter user (0-19): ")
+	projectedRatings = {}
+	for u in range( len(users) ):
+		#print "- Getting kNN for User #" + str(u)
+		NN = kNN( np.array( matrix[u] ), 4 )[1:]
+		weights = assignWeights( u, NN, matrix )
+		#print weights
+		#print "- Recommendations for User #" + str(u)
+		#print
+		#print "User #" + str(u) + "'s Nearest Neighbors and Weights:"
+		projectedRatings[u] = getRecItems(u, weights, users)
+
+	#print (time.time() - start_time)
+	
+	print projectedRatings
+	"""call = input("Enter user number: ")
+	print projectedRatings[call]"""
+
+	#print users
+
+
+	"""print "Angelica and Billie Jean: "
+	a = getOrderedValues( users.get("Angelica"), users.get("BillieJean") )
+	b = getOrderedValues( users.get("BillieJean"), users.get("Angelica") )
+	getSimilarity( np.array( a ), np.array( b ) )
+	print "\n\nAngelica and Candace: "
+	a = getOrderedValues( users.get("Angelica"), users.get("Candace") )
+	b = getOrderedValues( users.get("Candace"), users.get("Angelica") )
+	getSimilarity( np.array( a ), np.array( b ) )
+	print "\n\nBillie Jean and Candace: "
+	a = getOrderedValues( users.get("BillieJean"), users.get("Candace") )
+	b = getOrderedValues( users.get("Candace"), users.get("BillieJean") )
+	getSimilarity( np.array( a ), np.array( b ) )"""
+
+main()
